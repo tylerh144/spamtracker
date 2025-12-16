e513cd00492a016a4a03c767cbf777cb188a08e9
tylerh144 <tylerh144@nycstudents.net>
2025-12-16 08:49:55 -0500

    Add files via upload
    upload stuff

---
+++ spam.py
@@ -0,0 +1,58 @@
+import os, re
+
+
+class EmailData:
+    def __init__(self, sender_name, sender_email, date, body):
+        self.sender_name = sender_name
+        self.sender_email = sender_email
+        self.date = date
+        self.body = body
+        self.rep = 0
+
+    def __str__(self):
+        return f"{self.sender_name}\n{self.sender_email}\n{self.date}\n{self.body}\n\n"
+    
+
+
+path = os.path.dirname(os.path.abspath(__file__))
+print(path)
+
+
+emails = os.listdir(path + "\\Emails\\")
+print(emails)
+
+emailData = []
+
+for file in emails:
+    eml = open(path + "\\Emails\\" + file)
+
+    curLine = eml.readline()
+
+    while curLine.split(":")[0] != "Date":
+        curLine = eml.readline()
+    
+    date = curLine.split(": ")[1]
+
+    # date and from are ordered differently sometimes, return to start
+    eml.seek(0)
+
+
+
+    while curLine.split(":")[0] != "From":
+        curLine = eml.readline()
+
+    idx = curLine.index("<")
+    name = curLine[6:idx-1]
+
+    sender = curLine[idx+1:len(curLine)-2]
+
+
+    #collect data
+    emailData.append(EmailData(name, sender, date, "test"))
+
+    eml.close()
+
+#print emldata
+for eml in emailData:
+    print(eml)
+
